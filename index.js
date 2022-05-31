26c5f018e998538774874b094a3eafe9d4b789c4
Imari Childress <imaricodes@gmail.com>
2022-05-31 09:55:10 -0400

    Current state: working with quotes api

--- index.js
+++ index.js
@@ -2,41 +2,93 @@ require('dotenv').config();
 
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+const Redis = require('redis');
+const fetch = require("node-fetch");
+const DISCORD_TOKEN = process.env.BOT_TOKEN;
 
+const QUOTE_URL = "https://zenquotes.io/api/random";
 
-/** one way to reference permissions using flags */
+const redisClient = Redis.createClient();
 
-// const intents = [
-//     Intents.FLAGS.GUILDS,
-//     Intents.FLAGS.GUILD_MESSAGES
-// ]
+//permissions for bot
+const intents = [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES
+]
 
-/** another way to reference permissions using bitfield generated on developer page */
+let sadWords = ["sad", 
+    "depressed", 
+    "unhappy", 
+    "angry",
+     "miserable"]
 
-const intentsBitfield = new Intents(32509);
+let encouragements = [
+    "Cheer up!",
+    "Hang in there.",
+    "You are a great person / bot!"
+  ]
 
+// Create a new discord discordClient with permissions passed in (intents)
+const discordClient = new Client({intents});
 
- // Create a new client instance with intents option
-const client = new Client({intents: intentsBitfield});
+discordClient.login(DISCORD_TOKEN);
 
+discordClient.on('ready', () =>{
+    console.log('discord client ready to go!')
+    discordClient.on("messageCreate", message => {
+       console.log('message received');
 
+        if (message.author.bot) return;
+        if (message.type === 'dm') return;
 
-// const client = new Discord.Client();
-
-// When the client is ready, run this code (only once)
-// client.once('ready', () => {
-// 	console.log('Ready!');
-// });
-
-client.on('ready', () =>{
-    console.log('ready to go!')
+        if (message.content === '$inspire') {
+                    console.log('user typed $inspire');
+                    getQuote().then(quote => message.channel.send(quote))
+                }
+    })  
 })
 
-client.on('message', msg => {
-    if (msg.content === 'ping'){
-        msg.reply("pong");
+async function getQuote() {
+    let response;
+    let dataJSON;
+    let data;
+
+    try {
+         response = await fetch("https://zenquotes.io/api/random")
+         dataJSON = await response.json();
+         data = await dataJSON[0]["q"] + " -" + dataJSON[0]["a"]
+        return data;
+    } catch (err) {
+        console.errror(err);
     }
-})
+}
 
-// Login to Discord with your client's token
-client.login(process.env.BOT_TOKEN);
+
+function updateEncouragements (encouragingMessage) {
+    //push message to redis database
+
+}
+
+
+
+//  function createRedisSet () {     
+//      redisClient.set('quotes', 'quute3'), (err, reply) => {
+//         console.log(reply); // 3
+//       });
+
+
+    // redisClient.sadd(['quotes', 'quote1', 'quote2', 'quute3'], function(err, reply) {
+    //     console.log(reply); // 3
+    //   });
+// }
+
+// createRedisSet();
+
+
+
+// (async () => {
+//     redisClient.on('error', (err) => console.log('Redis Client Error', err));
+//     await redisClient.connect();
+// })
+
+
