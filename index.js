e7b95feedba11037ae62b52337a742131e957377
Imari Childress <imaricodes@gmail.com>
2022-07-24 01:25:45 -0400

    Add regex to validate city/state format

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const fetch = require("node-fetch");
 const redisClient = Redis.createClient();
 
 const DISCORD_TOKEN = process.env.BOT_TOKEN;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-const TEST_API_REQ = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=London&aqi=no`
+// const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+// const TEST_API_REQ = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=London&aqi=no`
 
 const OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY;
-const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=las vegas&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
+// const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=las vegas&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
 
-const QUOTE_URL = "https://zenquotes.io/api/random";
+const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=detroit,mi,usa&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
+
+
 
 
 //permissions for bot
@@ -38,23 +40,51 @@ discordClient.on('ready', () =>{
         if (message.author.bot) return;
         if (message.type === 'dm') return;
 
-        if (message.content === 's') {
-                    console.log('user typed $inspire');
-                    // console.log(fetchWeatherData());
+        if (message.content.startsWith('!')) {
+            console.log(message.content);
+            let cityState = validateCityStateFormat('detroit, mI');
+            // console.log('cats', cats);
+            let botMessage = 'If you\'d like to know the weather where you are, type your city and two letter state separated by a comma'
+            
+            //check that city state is in correct format
+            cityState ? console.log('correct format') : console.log('incorrect format');
+
+            //if format is correct, split into two strings: city and state
+
+            //validate city and state existence with usps api
+
+            //if city and state existence is valid (are actual locations) submit to weather api  fetchWeather() > return results
+
+            //if format is incorrect, notify user to try again with correctly formatted city state then return from this if block
+
+    
+            
+
+            // let textValidate = useRegex(message.content)
+            // console.log('validate', textValidate);
                     let weatherInfo = await fetchWeatherData();
-                    // console.log("line 53 weather info ", weatherInfo);
+                   
 
                     //convert weather info obj to returnable string.
                     // let botMessage = `The current weather in ${weatherInfo.cityName} is ${weatherInfo.weatherDescription}.`;
-                    let botMessage = `The current temperature in ${weatherInfo.cityName} is ${weatherInfo.currentTemp} degrees Fahrenheit.`;
+                    // let botMessage = `The current temperature in ${weatherInfo.cityName} is ${weatherInfo.currentTemp} degrees Fahrenheit.`;
+                    // let botMessage = `The current temperature in your zipcode is ${weatherInfo.currentTemp} degrees Fahrenheit.`;
 
                     // return weather info to discord
                      message.channel.send(botMessage);
 
+                     //clear weather info
                     weatherInfo = {};
                 }
     })  
 })
+
+
+//this regex test checks if there is a string followed by a comma (how to limit this to one?) folowed by a string of only two letters
+function validateCityStateFormat(input) {
+    let regex = /^[a-zA-Z\s]+,\s*[A-Z][A-Z]/; //matches city comma two cap letters for state
+    return regex.test(input);
+}
 
 // this detructures weatherReport obj
 function getWeatherDetails ({name, weather, main}) {
@@ -81,7 +111,7 @@ async function fetchWeatherData() {
         let weatherReport = getWeatherDetails(dataJSON);
         // console.log(dataJSON);
 
-        console.log("line 89 return to bot", weatherReport);
+        // console.log("line 89 return to bot", weatherReport);
         return weatherReport
 
     } catch (err) {
@@ -91,13 +121,6 @@ async function fetchWeatherData() {
 
 
 
-
-
-
-
-function updateEncouragements (encouragingMessage) {
-    //push message to redis database
-}
 
 
 
