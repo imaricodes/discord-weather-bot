d3913af489fa9e4b7f292c6fdf31dbd694ebd287
Imari Childress <imaricodes@gmail.com>
2022-08-05 20:55:11 -0400

    Saving progress before refactoring getCityStaet()

--- index.js
+++ index.js
@@ -11,64 +11,10 @@ const axios = require('axios').default
 //usps credentials
 const USERNAME = process.env.USPS_API_ID
 
-
-
-
-//TODO: How to handle nonexistent or incorrectly formatted zip code
-
-
-async function getCityState (userZip) {
-    
-    let root = xmlbuilder2.create({version: '1.0'})
-.ele('CityStateLookupRequest', {USERID: USERNAME})
-    .ele('ZipCode')
-        .ele('Zip5').txt(`${userZip}`).up()
-    .up()
-.up();
-
-let xml=root.end({prettyprint: true});
-
-
-    axios.get('https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&xml=' + encodeURIComponent(xml))
-    .then(function(response){
-        const userLocation = xmlbuilder2.convert(response.data, {format: 'object'});
-        // console.log(obj.CityStateLookupResponse.ZipCode.City);
-      
-        // let cityStateObj = {
-        //     "city": '',
-        //     "state": ''
-        // };
-        const {CityStateLookupResponse:{ZipCode:{State}}, CityStateLookupResponse:{ZipCode:{City}}} = userLocation;
-
-        let cityStateObj = {
-            city: City,
-            state: State
-        }
-        return cityStateObj;
-        console.log('city state obj: ', cityStateObj);
-        console.log('City and state are ', City, State);
-    })
-    .then ((cityStateObj)=>{
-        console.log('made it: ', cityStateObj);
-
-    })
-    .catch (function(error){
-        console.log(error);
-    });
-}
-
-
-getCityState('44118');
-/** END ZIP CODE API*/
-
-
-
-
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const Redis = require('redis');
 const fetch = require("node-fetch");
-const redisClient = Redis.createClient();
 
 const DISCORD_TOKEN = process.env.BOT_TOKEN;
 // const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
@@ -77,8 +23,128 @@ const DISCORD_TOKEN = process.env.BOT_TOKEN;
 const OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY;
 // const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=las vegas&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
 
+
+
+//TODO: How to handle nonexistent or incorrectly formatted zip code
+
+
+
+const getCityState = async (userZip) => {
+    
+    let root = xmlbuilder2.create({version: '1.0'})
+    .ele('CityStateLookupRequest', {USERID: USERNAME})
+        .ele('ZipCode')
+            .ele('Zip5').txt(`${userZip}`).up()
+        .up()
+    .up();
+
+    let xml=root.end({prettyprint: true});
+
+
+    const output = await axios.get('https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&xml=' + encodeURIComponent(xml))
+    .then(response => {
+        const userLocation = xmlbuilder2.convert(response.data, {format: 'object'});
+        // console.log('1 geCityState():',userLocation.CityStateLookupResponse.ZipCode.City);
+
+        const {CityStateLookupResponse:{ZipCode:{State}}, CityStateLookupResponse:{ZipCode:{City}}} = userLocation;
+
+        let cityStateObj = {
+            city: City,
+            state: State
+        }
+        console.log('city state obj: ', cityStateObj);
+        // console.log('City and state are ', City, State);
+        return cityStateObj;
+    })
+    
+    // .then((location) => {
+    //     console.log('zipzip: ', location);
+    //     fetchWeatherData(location)
+    // })
+    // .then ((cityStateObj)=>{
+    //     console.log('made it: ', cityStateObj);
+
+    // })
+    .catch (function(error){
+        console.log(error);
+    });
+}
+
+
+async function getWeather(userZip) {
+  await getCityState(userZip)
+  .then((data)=>{
+        
+        // fetchWeatherData(data)
+        console.log('get weather data: ', data);
+    })
+//    await fetchWeatherData(test);
+//    console.log('second');
+     
+    // .then((cityStateObj) => {
+    //     // console.log(cityStateObj);
+    //     fetchWeatherData(cityStateObj)
+    //    }) 
+    // .then((dataJSON) => {getWeatherDetails(dataJSON)}) //return weather details as object
+    // .then((data) => {
+    //     let weatherReport = data;
+    //     return weatherReport;
+    // })
+}
+
+getWeather('44118');
+
+// getCityState('44118')
+
+
+
+
 //TODO: replace city and state with template literal from usps api resuts (return city and state from axios fetch as an object?)
-const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=detroit,mi,usa&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
+// const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=detroit,mi,usa&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
+
+
+// this detructures weatherReport obj (NOT REALLY DESTRUCTURING??)
+async function getWeatherDetails ({name, weather, main}) {
+    let cityName = name;
+    let weatherDescription = weather[0].description
+    let currentTemp = main.temp
+    console.log('current temp', currentTemp);
+    // console.log(conditions);
+    return {cityName, weatherDescription, currentTemp}
+}
+
+async function fetchWeatherData(test) {
+    // console.log('2 fetchWeatherData() why undefined: ', test);
+    let response;
+    let dataJSON;
+    // let city = test.city.toLowerCase()
+    // let state = test.state.toLowerCase()
+    // console.log(city, state);
+
+    try {
+
+        console.log('test success: ', test);
+        
+        //  response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},usa&units=imperial&APPID=${OPEN_WEATHER_KEY}`);
+         response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=denver,co,usa&units=imperial&APPID=${OPEN_WEATHER_KEY}`);
+         
+         
+         dataJSON =  await response.json();
+         console.log(dataJSON);
+     
+          /**  OPENWEATHER DATA */
+        // const {name, weather} = dataJSON; //destructure
+
+        // let weatherReport = await getWeatherDetails(dataJSON);
+        // console.log(dataJSON);
+
+        // console.log("line 89 return to bot", weatherReport);
+        // return dataJSON
+
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 
 //permissions for bot
@@ -104,11 +170,11 @@ discordClient.on('ready', () =>{
                 console.log(message.content);
                 // let cityState = validateCityStateFormat('detroit, mi');
                 // console.log('cats', cats);
-                // let botMessage = 'If you\'d like to know the weather where you are, enter your 5 digit US zip code'
-                let weatherInfo = await fetchWeatherData();
+                let botMessage = 'If you\'d like to know the weather where you are, enter your 5 digit US zip code'
+                // let weatherInfo = await fetchWeatherData();
                     
                 //convert weather info obj to returnable string.
-                let botMessage = `The current weather in ${weatherInfo.cityName} is ${weatherInfo.weatherDescription}.`;
+                // let botMessage = `The current weather in ${weatherInfo.cityName} is ${weatherInfo.weatherDescription}.`;
                 // let botMessage = `The current temperature in ${weatherInfo.cityName} is ${weatherInfo.currentTemp} degrees Fahrenheit.`;
                 // let botMessage = `The current temperature in your zipcode is ${weatherInfo.currentTemp} degrees Fahrenheit.`;
 
@@ -116,48 +182,10 @@ discordClient.on('ready', () =>{
                 message.channel.send(botMessage);
 
                 //clear weather info
-                weatherInfo = {};
+                // weatherInfo = {};
         }
     })  
 })
-
-
-
-// this detructures weatherReport obj (NOT REALLY DESTRUCTURING??)
-function getWeatherDetails ({name, weather, main}) {
-    let cityName = name;
-    let weatherDescription = weather[0].description
-    let currentTemp = main.temp
-    console.log('current temp', currentTemp);
-    // console.log(conditions);
-    return {cityName, weatherDescription, currentTemp}
-}
-
-async function fetchWeatherData() {
-    let response;
-    let dataJSON;
-
-    try {
-         response = await fetch(OPEN_WEATHER_URL);
-         
-         dataJSON =  await response.json();
-     
-          /**  OPENWEATHER DATA */
-        // const {name, weather} = dataJSON; //destructure
-
-        let weatherReport = getWeatherDetails(dataJSON);
-        // console.log(dataJSON);
-
-        // console.log("line 89 return to bot", weatherReport);
-        return weatherReport
-
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-
-
 
 
 
